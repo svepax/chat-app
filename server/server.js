1a3f3697c889ff42d5fd21e039e3888e19029032
Sven Pannewitz <sven.pannewitz@gmail.com>
2017-11-11 08:25:51 +0100

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-    console.log(publicPath);
+  console.log(`Server is up on ${port}`);
 });
