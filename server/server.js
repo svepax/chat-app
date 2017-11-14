984078865c54bdbc5f8162eaef9617c6ea373371
Sven Pannewitz <sven.pannewitz@gmail.com>
2017-11-14 16:10:04 +0100

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,10 +13,28 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+    
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'Dieter joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
